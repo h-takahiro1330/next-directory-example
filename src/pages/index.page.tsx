378b7bd40313f1a0436f1515bd52fbac23d68f18
日置　貴大 <tkhr0410h@gmail.com>
2021-08-13 17:58:25 +0900

    add: prettier

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '~/styles/Home.module.css'
-
 export default function Home() {
-  return (
-    <h1>Next.js directory example</h1>
-  )
+  return <h1>Next.js directory example</h1>
 }
